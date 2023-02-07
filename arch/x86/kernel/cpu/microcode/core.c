01bd4486138ec018e1bb204d93e4e52190dfde21
Dave Hansen <dave.hansen@linux.intel.com>
2023-02-07 12:38:53 -0800

    [Patch v3 Part2 9/9] x86/microcode: Provide an option to override minrev enforcement
    From: Dave Hansen <dave.hansen@linux.intel.com>
    
    Minimum Required Revision (minrev) is enforced strictly. All new patches
    will have a minrev that is not zero. But there might be a transition time
    for some that need this enforcement to be relaxed.
    
    When the override is enabled, the kernel will be tainted.
    
    Provide a debugfs variable to override the minrev enforcement.
    
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: LKML <linux-kernel@vger.kernel.org>
    Cc: x86 <x86@kernel.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Alison Schofield <alison.schofield@intel.com>
    Cc: Reinette Chatre <reinette.chatre@intel.com>
    Cc: Thomas Gleixner (Intel) <tglx@linutronix.de>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Stefan Talpalaru <stefantalpalaru@yahoo.com>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Rafael J. Wysocki <rafael@kernel.org>
    Cc: Peter Zilstra (Intel) <peterz@infradead.org>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Andrew Cooper <Andrew.Cooper3@citrix.com>

--- arch/x86/kernel/cpu/microcode/core.c
+++ arch/x86/kernel/cpu/microcode/core.c
@@ -23,6 +23,7 @@
 #include <linux/miscdevice.h>
 #include <linux/capability.h>
 #include <linux/firmware.h>
+#include <linux/debugfs.h>
 #include <linux/kernel.h>
 #include <linux/delay.h>
 #include <linux/mutex.h>
@@ -43,7 +44,9 @@
 #define DRIVER_VERSION	"2.2"
 
 static struct microcode_ops	*microcode_ops;
+static struct dentry		*dentry_ucode;
 static bool dis_ucode_ldr = true;
+bool override_minrev;
 
 bool initrd_gone;
 
@@ -521,7 +524,11 @@ static ssize_t reload_store(struct device *dev,
 		pr_err("Attempting late microcode loading - it is dangerous and taints the kernel.\n");
 		pr_err("You should switch to early loading, if possible.\n");
 		ret = -EINVAL;
-		goto put;
+
+		if (!override_minrev)
+			goto put;
+
+		pr_info("Overriding minrev\n");
 	}
 
 	tmp_ret = microcode_ops->request_microcode_fw(bsp, &microcode_pdev->dev);
@@ -547,7 +554,7 @@ put:
 
 	/* Taint only when loading was successful */
 	if (load_success) {
-		if (!safe_late_load) {
+		if (!safe_late_load || override_minrev) {
 			pr_warn("Microcode late loading tainted the kernel\n");
 			add_taint(TAINT_CPU_OUT_OF_SPEC, LOCKDEP_STILL_OK);
 		}
@@ -720,7 +727,11 @@ static int __init microcode_init(void)
 	cpuhp_setup_state_nocalls(CPUHP_AP_ONLINE_DYN, "x86/microcode:online",
 				  mc_cpu_online, mc_cpu_down_prep);
 
+	dentry_ucode = debugfs_create_dir("microcode", NULL);
+	debugfs_create_bool("override_minrev", 0644, dentry_ucode, &override_minrev);
+
 	pr_info("Microcode Update Driver: v%s.", DRIVER_VERSION);
+	pr_info("Override minrev %s\n", override_minrev ? "enabled" : "disabled");
 
 	return 0;
 
