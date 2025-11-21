b65cf2958089707b159769dcd38c4f21bf1e59d7
Dave Hansen <dave.hansen@linux.intel.com>
2025-11-20 16:33:59 -0800

    x86/cpu: Break Vendor/Family/Model macros into separate header
    The intel-family.h header uses Vendor/Family/Model macros but it does not
    #include the header where they are defined. If that header is included,
    the build blows up in #include hell.
    
    Luckily, these macros are completely independent and do not themselves
    have any dependencies on other code.
    
    Break the VFM_*() macros out into their own header.
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>

--- arch/x86/include/asm/cpu_device_id.h
+++ arch/x86/include/asm/cpu_device_id.h
@@ -2,38 +2,7 @@
 #ifndef _ASM_X86_CPU_DEVICE_ID
 #define _ASM_X86_CPU_DEVICE_ID
 
-/*
- * Can't use <linux/bitfield.h> because it generates expressions that
- * cannot be used in structure initializers. Bitfield construction
- * here must match the union in struct cpuinfo_86:
- *	union {
- *		struct {
- *			__u8	x86_model;
- *			__u8	x86;
- *			__u8	x86_vendor;
- *			__u8	x86_reserved;
- *		};
- *		__u32		x86_vfm;
- *	};
- */
-#define VFM_MODEL_BIT	0
-#define VFM_FAMILY_BIT	8
-#define VFM_VENDOR_BIT	16
-#define VFM_RSVD_BIT	24
-
-#define	VFM_MODEL_MASK	GENMASK(VFM_FAMILY_BIT - 1, VFM_MODEL_BIT)
-#define	VFM_FAMILY_MASK	GENMASK(VFM_VENDOR_BIT - 1, VFM_FAMILY_BIT)
-#define	VFM_VENDOR_MASK	GENMASK(VFM_RSVD_BIT - 1, VFM_VENDOR_BIT)
-
-#define VFM_MODEL(vfm)	(((vfm) & VFM_MODEL_MASK) >> VFM_MODEL_BIT)
-#define VFM_FAMILY(vfm)	(((vfm) & VFM_FAMILY_MASK) >> VFM_FAMILY_BIT)
-#define VFM_VENDOR(vfm)	(((vfm) & VFM_VENDOR_MASK) >> VFM_VENDOR_BIT)
-
-#define	VFM_MAKE(_vendor, _family, _model) (	\
-	((_model) << VFM_MODEL_BIT) |		\
-	((_family) << VFM_FAMILY_BIT) |		\
-	((_vendor) << VFM_VENDOR_BIT)		\
-)
+#include <asm/vfm.h>
 
 /*
  * Declare drivers belonging to specific x86 CPUs
